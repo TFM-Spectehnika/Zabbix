2b6ba71e60abf3e6d1e943582a351b0354eb7f29
Kurlis Andrey <94551812+Warikos@users.noreply.github.com>
2021-11-28 16:44:29 +0700

    Add files via upload

---
+++ install_zabbix.ps1
@@ -0,0 +1,44 @@
+#Set variables
+$RemoteZabbixPath = '\\srv-db1\config.1c$\zabbix\zabbix_work\'
+$LocalZabbixPath = 'C:\Zabbix\'
+$LocalZabbixConf = 'C:\Zabbix\zabbix_agentd.conf'
+$Zabbix = 'Zabbix Agent'
+
+#Function install zabbix like service
+function Install_zabbix {
+& C:\Zabbix\zabbix_agentd.exe -i -c $LocalZabbixConf
+}
+
+#Function start Zabbix Service
+function Run_Zabbix {
+Start-Service $Zabbix
+}
+
+#Function stop Zabbix Service
+function Stop_Zabbix {
+    Stop-Service $Zabbix
+    }
+
+
+#Check path exist and changes in .conf 
+If (!(Test-Path -Path $LocalZabbixPath)) {
+    Copy-Item $RemoteZabbixPath $LocalZabbixPath -Recurse -Force
+    Install_zabbix
+    Run_Zabbix
+    Exit
+    }
+else {
+    $Rem = Get-FileHash '\\srv-db1\config.1c$\zabbix\zabbix_work\zabbix_agentd.conf' -Algorithm SHA256 
+    $loc = Get-FileHash 'C:\Zabbix\zabbix_agentd.conf' -Algorithm SHA256
+    If ($Rem.Hash -ne $loc.Hash) {
+        Stop_Zabbix
+        Remove-Item -Path $LocalZabbixPath -Force -Recurse
+        Copy-Item $RemoteZabbixPath $LocalZabbixPath -Recurse -Force
+        Install_zabbix
+        Run_Zabbix
+        Exit
+        }
+    Else {Exit}
+}
+
+Exit
